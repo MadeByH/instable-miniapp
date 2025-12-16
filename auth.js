6d3e313480b49eca9fe5f8a27eda7b229c53f3c1
MadeByH <mahmmodihossein523@gmail.com>
2025-12-16 16:51:51 +0330

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,14 +1,26 @@
 async function ensureRegistered() {
-  if (!window.Bale || !window.Bale.WebApp)
+  if (!window.Bale || !window.Bale.WebApp) {
     throw new Error("NOT_IN_BALE");
+  }
 
   await waitForBaleUser();
 
   const userId = getUserId();
-  if (!userId)
+  if (!userId) {
     throw new Error("NO_USER");
+  }
 
-  const res = await fetch(`${API_BASE}/user_exists/${userId}`);
+  let res;
+  try {
+    res = await fetch(`${API_BASE}/user_exists/${userId}`);
+  } catch {
+    throw new Error("API_DOWN");
+  }
+
+  if (!res.ok) {
+    throw new Error("API_ERROR");
+  }
+
   const data = await res.json();
 
   if (!data.exists) {
